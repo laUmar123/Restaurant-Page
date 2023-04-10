aad233c91b92f42d3600bb8b0a43b4e1b19c4e02
Umar <work.umar7@hotmail.com>
2023-04-10 18:27:02 +0100

    added style to setup the header
    changed the bundled js file name to bundle.js, added relevant assets and also created a stylesheet and added the style for the page header

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,25 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     devtool: 'inline-source-map',
-    watch: true
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 }
