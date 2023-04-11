267b5a5b26389a3fe84e4a527cc01bd182ecedf7
Umar <work.umar7@hotmail.com>
2023-04-12 00:42:06 +0100

    created two functions that generate the page title and navbar

--- src/homePage.js
+++ src/homePage.js
@@ -3,3 +3,42 @@ const generateHeader = function () {
     return header;
 };
 
+/**
+ * this function creates a h1 element, adds the title class to it and sets the inner html to the taste of pakistan
+ * @returns the title DOM element
+ */
+const generateTitle = function () {
+    const title = document.createElement('h1');
+    title.classList.add('title');
+    title.innerHTML = "The Taste of Pakistan";
+    return title;
+};
+
+/**
+ * generates a div element with 3 nav items within it
+ * @returns a div element that contains all nav items
+ */
+const generateNav = function () {
+    const nav = document.createElement('div');
+    nav.classList.add('nav');
+    const navItemOne = document.createElement('div');
+    navItemOne.classList.add('nav-item');
+    navItemOne.id = 'home'
+    navItemOne.classList.add('active');
+    navItemOne.innerHTML = 'HOME';
+    const navItemTwo = document.createElement('div');
+    navItemTwo.innerHTML = 'MENU';
+    navItemTwo.id = 'menu';
+    const navItemThree = document.createElement('div');
+    navItemThree.innerHTML = 'CONTACT';
+    navItemThree.id = 'contact';
+    navItemOne.classList.add('nav-item');
+    navItemTwo.classList.add('nav-item');
+    navItemThree.classList.add('nav-item');
+    nav.append(navItemOne);
+    nav.append(navItemTwo);
+    nav.append(navItemThree);
+    return nav;
+};
+
+export { generateHeader };
--- src/imageChange.js
+++ src/imageChange.js
@@ -4,13 +4,16 @@ import imageThree from './assets/imageThree.jpg'
 
 const images = [imageOne, imageTwo, imageThree];
 
+/**
+ * this function changes the background image to the next image in the array every 10 seconds
+ */
 function changeBackground() {
     const mainPage = document.getElementById('content');
     let i = 0;
     setInterval(function () {
         mainPage.style.backgroundImage = `url("${images[i]}")`;
         i = i + 1;
-        if (i == images.length) {
+        if (i === images.length) {
             i = 0;
         }
     }, 10000);
